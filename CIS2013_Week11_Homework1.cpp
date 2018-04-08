e2b9d74957346edcfd8503c9c64b565ee376614e
Natasha S Love <debora@inbox.ru>
2018-04-08 18:22:44 -0500

    all in public

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -4,21 +4,60 @@ using namespace std;
 // the class will remember all data changes in the main
 class Car {
 	
-	private:
+	/*private:
 		string color = "n/a";
 		string make = "n/a";
 		string model = "n/a";
 		int year = 0;
 		int current_speed = 0;
+		*/
 	public:
-		void print () {
-			cout <<"You are driving a " << year <<  " " << model << " " << make << " " << color << " with current speed: " << current_speed << endl;
-		}
-		void set (string c, string m, string o, int y) {
+		/*void set (string c, string m, string o, int y) {
 			year = y;
 			color = c;
 			make = m;
 			model = o;
+		}*/
+		
+		string color = "n/a";
+		string make = "n/a";
+		string model = "n/a";
+		int year = 0;
+		int current_speed = 0;
+		
+		void print () {
+			// string c;
+			// string m;
+			// string o;
+			// int y;
+			cout <<"You are driving a " << year <<  " " << model << " " << make << " " << color << " with current speed: " << current_speed << endl;
+		}
+		
+		
+		void set_make () {
+			//string m;
+			//make = m;
+			cout << "Set make of your car : " << endl;
+			cin >> make;
+			
+		}
+		void set_model () {
+			//string o;
+			cout << "Set model of your car : " << endl;
+			cin >> model;
+				
+		}
+		void set_color () {
+			//string c;
+			cout << "Set color of your car : " << endl;
+			cin >> color;
+				
+		}
+		void set_year () {
+			//int y;
+			cout << "Set year of your car : " << endl;
+			cin >> year;
+				
 		}
 		/*int get_year () {
 			return year; // create this function to access year from private class
@@ -31,7 +70,15 @@ class Car {
 
 int main () {
 	
+	// char c, o, m;
+	// int y;
 	Car toyota;
+	
+	toyota.print ();
+	toyota.set_color ();
+	toyota.set_model ();
+	toyota.set_make ();
+	toyota.set_year ();
 	
 	toyota.print ();
 	
