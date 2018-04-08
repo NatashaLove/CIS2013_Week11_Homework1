dc6631bf7f7b229b47dff03dec40176e31719f5f
Natasha S Love <debora@inbox.ru>
2018-04-08 18:39:52 -0500

    main functions created

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -59,6 +59,67 @@ class Car {
 			cin >> year;
 				
 		}
+		void start () {
+			if (current_speed==0) {
+				current_speed = 25;
+			} else {
+				cout << "The car is already driving! " << endl;
+				
+			}
+		}
+		void stop () {
+			if (current_speed == 0) {
+				cout << "The car is already off! " << endl;
+			} else {
+				if (current_speed >> 20) {
+					cout << " Slow down ! "<< endl;
+				}
+				else {
+				current_speed = 0;
+				}
+			}
+		}
+		void drive () {
+			if (current_speed == 0) {
+				cout << "The car is off! Start the car before driving! " << endl;
+			} 
+		}
+		void speed_up () {
+			if (current_speed == 0) {
+				cout << "The car is off! Start the car before driving! " << endl;
+			} else {
+				current_speed=+10;
+			
+			}
+		}
+		void slow_down () {
+			if (current_speed == 0) {
+				cout << "The car is off! Start the car before driving! " << endl;
+			} else {
+				current_speed=-10;
+			
+			}
+		}
+		void turn_left () {
+			if (current_speed == 0) {
+				cout << "The car is off! Start the car before driving! " << endl;
+				} else {
+				current_speed=-10;
+			
+			cout << "The left signal is on. Turning left. " << endl;
+				}
+		}
+		void turn_right () {
+			if (current_speed == 0) {
+				cout << "The car is off! Start the car before driving! " << endl;
+				} else {
+				current_speed=-10;
+			
+			cout << "The right signal is on. Turning right. " << endl;
+				}
+			
+			
+		}
 		/*int get_year () {
 			return year; // create this function to access year from private class
 		}
