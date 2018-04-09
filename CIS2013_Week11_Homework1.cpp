a517f6b7b64b50ed31fba7e251a0140dbe82f158
Natasha S Love <debora@inbox.ru>
2018-04-08 20:52:19 -0500

    optimizing

--- CIS2013_Week11_Homework1.cpp
+++ CIS2013_Week11_Homework1.cpp
@@ -62,6 +62,7 @@ class Car {
 		void start () {
 			if (current_speed==0) {
 				current_speed = 25;
+				cout << "The car is started and driving at the speed " << current_speed<< endl;
 			} else {
 				cout << "The car is already driving! " << endl;
 				
@@ -71,59 +72,68 @@ class Car {
 			if (current_speed == 0) {
 				cout << "The car is already off! " << endl;
 			} else {
-				if (current_speed >> 20) {
+				if (current_speed > 20) {
 					cout << "Your current speed is " << current_speed << " Slow down ! "<< endl;
 				}
 				else {
 				current_speed = 0;
+				cout << "The car has stopped. " << endl;
 				}
 			}
 		}
 		void drive () {
 			if (current_speed == 0) {
 				cout << "The car is off! Start the car before driving! " << endl;
+			} else {
+				cout << "You are driving at the speed "<< current_speed << endl;
 			} 
 		}
 		void speed_up () {
 			if (current_speed == 0) {
 				cout << "The car is off! Start the car before driving! " << endl;
 			} else {
-				current_speed=+10;
+				current_speed+=10;
 				cout << "Your current speed is " << current_speed << endl;
 			
 			}
 		}
 		void slow_down () {
-			if (current_speed == 0) {
-				cout << "The car is off! Start the car before driving! " << endl;
-			} else {
-				current_speed=-10;
+			if (current_speed > 0) {
+				//for (int i=current_speed; i>=0; --i)
+				current_speed-=5;
 				cout << "Your current speed is " << current_speed << endl;
-			
+				
+			} else {
+				cout << "The car is off! Start the car before driving! " << endl;
 			}
 		}
 		void turn_left () {
 			if (current_speed == 0) {
 				cout << "The car is off! Start the car before driving! " << endl;
-				} else {
-					for (int i=current_speed; i>=10; i--)
-				//current_speed=-10;
-			
-				cout << "The left signal is on. Turning left. " << endl;
-				cout << "Your current speed is " << current_speed << endl;
+				} else if (current_speed > 30) {
+						cout << << "Your current speed is " << current_speed << " Slow down ! "<< endl;
+					} else {
+					
+					for (int i=current_speed; i >= 15; i-=5)
+						current_speed = i;
+				//cout << "Your speed is now " << current_speed<< endl;
 				}
+		cout << "The left signal is on. Turning left. " << endl;
+		cout << "Your current speed is " << current_speed << endl;
 		}
 		void turn_right () {
 			if (current_speed == 0) {
 				cout << "The car is off! Start the car before driving! " << endl;
+				} else if (current_speed > 30) {
+						cout << << "Your current speed is " << current_speed << " Slow down ! "<< endl;
 				} else {
-					for (int i=current_speed; i>=10; i--)
-				//current_speed=-10;
-			
-			cout << "The right signal is on. Turning right. " << endl;
-			cout << "Your current speed is " << current_speed << endl;
+					for (int i=current_speed; i >=15; i-=5)
+					current_speed = i;
+				//cout << "Your speed is now " << current_speed<< endl;
+		
 				}
-			
+		cout << "The right signal is on. Turning right. " << endl;
+		cout << "Your current speed is " << current_speed << endl;	
 			
 		}
 		/*int get_year () {
@@ -137,20 +147,115 @@ class Car {
 
 int main () {
 	
-	// char c, o, m;
-	// int y;
+	 char c, o, m, y, s, S, p, D, l, r, u, d; 
+	 // where : c = color, o = model, m = make, y = year, s = start, S=STOP, p = print, D= DRIVE, l= turn left, r = turn right, u= speed up, d = slow down; 
+	char choice;
 	Car car;
 	
-	car.print ();
+	cout << endl;
+	cout << "***********************************************" << endl;
+	cout << endl;
+	cout << "Welcome to our test drive! " << endl;
+	cout << endl;
+	cout << " " << endl;
+	cout << "Set your car's color (c) "<< endl;
+	cout << "Set your car's model (o) " << endl;
+	cout << "Set your car's make (m) " << endl;
+	cout << "Set your car's year (y) " << endl;
+	cout << "Print your car's settings (p) " << endl;
+	cout << endl;
+	cout << " " << endl;
+	
 	car.set_color ();
 	car.set_model ();
 	car.set_make ();
 	car.set_year ();
-	car.start ();
-	car.speed_up ();
-	car.turn_left ();
-	car.stop ();
 	car.print ();
+	
+	do {
+	cout << " " << endl;
+	cout << "Select an action: " << endl;
+	cout << " " << endl;
+	cout << "Set your car's color (c) "<< endl;
+	cout << "Set your car's model (o) " << endl;
+	cout << "Set your car's make (m) " << endl;
+	cout << "Set your car's year (y) " << endl;
+	cout << "Print your car's settings (p) " << endl;
+	cout << "		*****		" << endl;
+	cout << "Start the car (s) " << endl;
+	cout << "Stop the car (S) " << endl;
+	cout << "Drive the car (D) " << endl;
+	cout << "Turn left (l) " << endl;
+	cout << "Turn right (r) " << endl;
+	cout << "Speed up (u) " << endl;
+	cout << "Slow down (d) " << endl;
+	cout << " " << endl;
+	
+	
+	cin >> choice;
+		switch (choice)
+		{
+		case 'c' :
+		car.set_color ();
+		break;
+		case 'o' :
+		car.set_model ();
+		break;
+		case 'm' :
+		car.set_make ();
+		break;
+		case 'p' :
+		car.print ();
+		break;
+		case 'y' :
+		car.set_year ();
+		break;
+		case 's' :
+		car.start ();
+		break;
+		case 'S' :
+		car.stop ();
+		break;
+		case 'D' :
+		car.drive ();
+		break;
+		case 'l' :
+		car.turn_left ();
+		break;
+		case 'r' :
+		car.turn_right ();
+		break;
+		case 'u' :
+		car.speed_up ();
+		break;
+		case 'd' :
+		car.slow_down ();
+		break;
+		
+		default : 
+		cout << "Illegal value.";
+		}
+	}
+	while (1);
+	/*if (choice == c)
+	car.set_color ();
+	if (choice == o)
+	car.set_model ();	
+	if (choice == m)
+	car.set_make ();
+	if (choice == p)
+	car.print ();
+	*/
+	// car.print ();
+	// car.set_color ();
+	// car.set_model ();
+	// car.set_make ();
+	// car.set_year ();
+	// car.start ();
+	// car.speed_up ();
+	// car.turn_left ();
+	// car.stop ();
+	// car.print ();
 	
 	return 0;
 	}
